e5016d0a886dc135bdb479439e3712c78000bf67
Patrick Kubiak <epicpatka@gmail.com>
2024-11-03 15:35:04 -0500

    update SecurityConfig.java

--- src/main/java/org/example/SecurityConfig.java
+++ src/main/java/org/example/SecurityConfig.java
@@ -23,13 +23,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/images/**", "/css/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .oauth2Login(withDefaults())
-                .logout(logout -> logout
-                        .addLogoutHandler(logoutHandler()));
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/", "/images/**", "/css/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .oauth2Login(withDefaults())
+            .logout(logout -> logout
+                .addLogoutHandler(logoutHandler()));
         return http.build();
     }
 
