898ef5a389d527b8b28354b52214952144a11e80
Patrick Kubiak <epicpatka@gmail.com>
2024-11-03 23:26:20 -0500

    Feature/unit tests (#2)
    * Unit test HomeController
    
    * Run unit tests in github actions
    
    * Generate jacoco coverage reports
    
    * Unit test main classes
    
    * Update all unit tests

---
+++ src/test/java/org/example/DemoApplicationTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class DemoApplicationTest {
+
+    @Test
+    public void main() {
+        // Act
+        DemoApplication.main(new String[] {});
+    }
+
+}
