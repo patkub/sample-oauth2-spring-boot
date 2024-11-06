468af694350b3ac5018c5e8cf5beec54550f6e08
Patrick Kubiak <epicpatka@gmail.com>
2024-11-05 20:19:45 -0500

    Setup webflux

--- mvc-login/exec.ps1
+++ mvc-login/exec.ps1
@@ -1,2 +1,2 @@
-podman build -t sample-oauth2-spring-boot .
-podman run -p 3000:3000 -it sample-oauth2-spring-boot
+podman build -t sample-oauth2-spring-boot-mvc .
+podman run -p 3000:3000 -it sample-oauth2-spring-boot-mvc
--- mvc-login/exec.sh
+++ mvc-login/exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-podman build -t sample-oauth2-spring-boot .
-podman run -p 3000:3000 -it sample-oauth2-spring-boot
+podman build -t sample-oauth2-spring-boot-mvc .
+podman run -p 3000:3000 -it sample-oauth2-spring-boot-mvc
